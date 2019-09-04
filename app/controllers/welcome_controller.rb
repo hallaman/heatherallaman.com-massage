3e2f19d66163794642fd24ace5e33df630bd307b
hallaman <heather.allaman@gmail.com>
2019-09-04 14:06:59 -0500

    schedule changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   def index
 
   	@testimonials = Testimonial.where(show: true)
-  	
+
     # client = Instagram.client(:access_token => '1517317818.1677ed0.daa3778c68b149cc82e9608c931a59d6')
     # @insta = client.present? ? client.user_recent_media : ''
 
